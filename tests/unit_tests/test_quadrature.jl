4a98f3992bd4c953dcc0faa7d564257fcce7af1a
Jack Hamel <jackhaml16@gmail.com>
2020-09-14 15:48:20 -0600

    Quadrature tests all pass now

--- tests/unit_tests/test_quadrature.jl
+++ tests/unit_tests/test_quadrature.jl
@@ -5,44 +5,43 @@ include("../../src/quadrature.jl")
 @testset "quadrature tests" begin
     @testset "gaussQuadrature tests" begin
         @testset "gaussQuadrature points and weights tests" begin
-            gauss7points_reference = [3.33333333333333314829616256247391e-01 3.33333333333333314829616256247391e-01 3.33333333333333314829616256247391e-01;
-                                      0.00000000000000000000000000000000e+00 0.00000000000000000000000000000000e+00 1.00000000000000000000000000000000e+00;
-                                      0.00000000000000000000000000000000e+00 1.00000000000000000000000000000000e+00 0.00000000000000000000000000000000e+00;
-                                      1.00000000000000000000000000000000e+00 0.00000000000000000000000000000000e+00 0.00000000000000000000000000000000e+00;
-                                      5.00000000000000000000000000000000e-01 5.00000000000000000000000000000000e-01 0.00000000000000000000000000000000e+00;
-                                      5.00000000000000000000000000000000e-01 0.00000000000000000000000000000000e+00 5.00000000000000000000000000000000e-01;
-                                      0.00000000000000000000000000000000e+00 5.00000000000000000000000000000000e-01 5.00000000000000000000000000000000e-01]
-            gauss7weights_reference = [4.50000000000000011102230246251565e-01
-                                       5.00000000000000027755575615628914e-02
-                                       5.00000000000000027755575615628914e-02
-                                       5.00000000000000027755575615628914e-02
-                                       1.33333333333333331482961625624739e-01
-                                       1.33333333333333331482961625624739e-01
-                                       1.33333333333333331482961625624739e-01]
-            @test_skip gauss7points == gauss7points_reference
-            @test_skip gauss7weights == gauss7weights_reference
+            gauss7points_reference = [3.33333333333333314829616256247391e-01 3.33333333333333314829616256247391e-01 3.33333333333333314829616256247391e-01
+                                      4.70142064105100010440452251714305e-01 4.70142064105100010440452251714305e-01 5.97158717897999791190954965713900e-01
+                                      4.70142064105100010440452251714305e-01 5.97158717897999791190954965713900e-02 4.70142064105100010440452251714305e-01
+                                      5.97158717897999791190954965713900e-02 4.70142064105100010440452251714305e-01 4.70142064105100010440452251714305e-01
+                                      1.01286507323455995943639607048681e-01 1.01286507323455995943639607048681e-01 7.97426985353087980357145170273725e-01
+                                      1.01286507323455995943639607048681e-01 7.97426985353087980357145170273725e-01 1.01286507323455995943639607048681e-01
+                                      7.97426985353087980357145170273725e-01 1.01286507323455995943639607048681e-01 1.01286507323455995943639607048681e-01]
+            gauss7weights_reference = [2.25000000000000255351295663786004e-01
+                                       1.32394152788506136442236993389088e-01
+                                       1.32394152788506136442236993389088e-01
+                                       1.32394152788506136442236993389088e-01
+                                       1.25939180544827139529573400977824e-01
+                                       1.25939180544827139529573400977824e-01
+                                       1.25939180544827139529573400977824e-01]
+            @test gauss7points == gauss7points_reference
+            @test gauss7weights == gauss7weights_reference
         end
         @testset "gaussQuadrature function tests" begin
             returnZero(x,y,z) = 0
             points = [0.0 0.0 0.0]; weights = [1.0]
             @test gaussQuadrature(returnZero, points, weights) == 0
+
             returnXplusYplusZ(x,y,z) = x + y + z
             points = [-1.5 0.5 0.0; 0.0 1.0 2.0]; weights = [1.0, 9.1]
             solution = 26.299999999999997
             @test gaussQuadrature(returnXplusYplusZ, points, weights) == solution
+
             # This test will integrate over a triangle using the 7 point rule above
-            f(x,y,z) = 2*x #+ y
-            nodes = [0.0 0.0 0.0; 0.0 2.0 0.0; 2.0 0.0 0.0]
+            f(x,y,z) = 2*x
+            nodes = [0.0 0.0 0.0; 2.0 0.0 0.0; 0.0 2.0 0.0]
             gauss7points_cartesian = Array{Float64, 2}(undef, 7, 3)
             for point_idx in 1:7
                 gauss7points_cartesian[point_idx,:] = barycentric2Cartesian(nodes, gauss7points[point_idx,:])
             end
-            println(gauss7points_cartesian)
-            solution = 8/3
             area = 2
-            # h=sqrt((2/3)^2+(2-2/3)^2)
-            @test area*gaussQuadrature(f, gauss7points_cartesian, gauss7weights) == solution
-            # @test 3/4*sqrt(3)*h^2*gaussQuadrature(f, gauss7points_cartesian, gauss7weights) == solution
+            solution = 8/3/area
+            @test gaussQuadrature(f, gauss7points_cartesian, gauss7weights) == solution
         end
     end
 
