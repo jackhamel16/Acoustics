a34f86be888398b0bb54615c7837cbcc78a1411c
Jack Hamel <jackhaml16@gmail.com>
2020-09-18 12:09:46 -0600

    Added all tests for computing the singular integral parameters.

--- src/greens_functions.jl
+++ src/greens_functions.jl
@@ -5,14 +5,14 @@ function scalarGreens(R::Float64, k::Complex{Float64})
 end
 
 function singularScalarGreens(d::Float64,
-                                P0_hat::Array{Float64, 2},
-                                u_hat::Array{Float64, 2},
-                                P0::Array{Float64, 1},
-                                R0::Array{Float64, 1},
-                                R_plus::Array{Float64, 1},
-                                R_minus::Array{Float64, 1},
-                                l_plus::Array{Float64, 1},
-                                l_minus::Array{Float64, 1})
+                              P0_hat::Array{Float64, 2},
+                              u_hat::Array{Float64, 2},
+                              P0::Array{Float64, 1},
+                              R0::Array{Float64, 1},
+                              R_plus::Array{Float64, 1},
+                              R_minus::Array{Float64, 1},
+                              l_plus::Array{Float64, 1},
+                              l_minus::Array{Float64, 1})
     integral = 0.0
     for i in 1:3 #i indicates triangle edge index
         integral += dot(P0_hat[i,:], u_hat[i,:]) *
@@ -23,4 +23,16 @@ function singularScalarGreens(d::Float64,
     integral
 end
 
-# function computeSingularParameters()
+function computeSingularScalarGreensParameters(r_test, nodes)
+    # r_test is the observation point
+    # nodes are the three triangle nodes
+    r_minus = nodes # beginning points of edges
+    r_plus = circshift(nodes, (-1,0)) # ending points of edges
+
+    normal_non_unit = cross(r_plus[1,:]-r_minus[1,:], r_minus[3,:]-r_plus[3,:])
+    normal = normal_non_unit / norm(normal_non_unit)
+
+    d = dot(normal, r_test - r_plus[1,:])
+
+    (d)#, P0_hat, u_hat, P0)#, R0, R_plus, R_minus, l_plus, l_minus)
+end
--- tests/unit_tests/test_greens_functions.jl
+++ tests/unit_tests/test_greens_functions.jl
@@ -1,4 +1,5 @@
 using Test
+using LinearAlgebra
 
 include("../../src/greens_functions.jl")
 
@@ -22,5 +23,50 @@ include("../../src/greens_functions.jl")
         solution = -2.09660929985
         @test isapprox(singularScalarGreens(d, P0_hat, u_hat, P0, R0, R_plus,
                                             R_minus, l_plus, l_minus), solution)
+    end
+    @testset "computeSingularScalarGreensParameters tests" begin
+        r_test = [2.0, 0.0, 1.0]
+        nodes = [0.0 0.0 0.0; 2.0 0.0 0.0; 0.0 2.0 1.0]
+
+        r_plus = circshift(nodes, (-1,0))
+        r_minus = nodes
+        n_hat = cross(r_plus[1,:]-r_minus[1,:],r_minus[3,:]-r_plus[3,:])/
+                norm(cross(r_plus[1,:]-r_minus[1,:],r_minus[3,:]-r_plus[3,:]))
+        d = dot(n_hat, r_test - r_plus[1,:])
+
+        rho_test = r_test - d * n_hat
+
+        rho_plus = Array{Float64, 2}(undef, 3, 3)
+        rho_minus = Array{Float64, 2}(undef, 3, 3)
+        I_hat = Array{Float64, 2}(undef, 3, 3)
+        u_hat = Array{Float64, 2}(undef, 3, 3)
+        l_plus = Array{Float64, 1}(undef, 3)
+        l_minus = Array{Float64, 1}(undef, 3)
+        P0 = Array{Float64, 1}(undef, 3)
+        for i in 1:3 # i is triangle edge index
+            rho_plus[i,:] = r_plus[i,:] - n_hat*dot(n_hat, r_plus[i,:])
+            rho_minus[i,:] = r_minus[i,:] - n_hat*dot(n_hat, r_minus[i,:])
+            I_hat[i,:] = (rho_plus[i,:]-rho_minus[i,:])/norm(rho_plus[i,:]-rho_minus[i,:])
+            u_hat[i,:] = cross(I_hat[i,:], n_hat)
+            l_plus[i] = dot(rho_test-r_plus[i,:], I_hat[i,:])
+            l_minus[i] = dot(rho_test-r_minus[i,:], I_hat[i,:])
+            P0[i] = abs(dot(rho_plus[i,:]-rho_test, u_hat[i,:]))
+        end
+
+        params = computeSingularScalarGreensParameters(r_test, nodes)
+        @test params[1] == d
+        @test params[1] == dot(n_hat, r_test - r_minus[1,:]) # another way to compute d
+        for i in 1:3 # i is triangle edge index
+            @test_skip params[2,i,:] == (dot(rho_plus[i,:]-rho_test, u_hat[i,:]) - l_plus[i]*I_hat[i,:]) / P0 # P0_hat
+            @test_skip params[2,i,:] == (dot(rho_minus[i,:]-rho_test, u_hat[i,:]) - l_minus[i]*I_hat[i,:]) / P0  # another way to compute P0_hat for edge 2
+            @test_skip params[3,i,:] == u_hat[i,:]
+            @test_skip params[4,i] == P0[i]
+            @test_skip params[4,i] == abs(dot(rho_minus[i,:]-rho_test, u_hat[i,:])) #another way to compute P0
+            @test_skip params[5,i] == sqrt(P0[i]^2 + d^2) #R0
+            @test_skip params[6,i] == sqrt(P_plus[i]^2 + d^2) #R_plus
+            @test_skip params[7,i] == sqrt(P_minus[i]^2 + d^2) #R_minus
+            @test_skip params[8,i] == l_plus[i]
+            @test_skip params[9,i] == l_minus[i]
+        end
     end
 end
