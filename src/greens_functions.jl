312b3db72ac3997a1496c758dd5482a15dcde4ed
Jack Hamel <jackhaml16@gmail.com>
2020-09-18 09:51:56 -0600

    the extracted singular integral has been added

--- src/greens_functions.jl
+++ src/greens_functions.jl
@@ -1,8 +1,26 @@
-#module greens_functions
-#export scalar_greens
+using LinearAlgebra
 
-function scalar_greens(R,k)
+function scalarGreens(R::Float64, k::Complex{Float64})
     exp(-im*k*abs(R))/(4*pi*abs(R))
 end
 
-#end # module
+function singularScalarGreens(d::Float64,
+                                P0_hat::Array{Float64, 2},
+                                u_hat::Array{Float64, 2},
+                                P0::Array{Float64, 1},
+                                R0::Array{Float64, 1},
+                                R_plus::Array{Float64, 1},
+                                R_minus::Array{Float64, 1},
+                                l_plus::Array{Float64, 1},
+                                l_minus::Array{Float64, 1})
+    integral = 0.0
+    for i in 1:3 #i indicates triangle edge index
+        integral += dot(P0_hat[i,:], u_hat[i,:]) *
+                    (P0[i] * log((R_plus[i]+l_plus[i])/(R_minus[i]+l_minus[i]))-
+                     abs(d) * (atan(P0[i]*l_plus[i], R0[i]^2+abs(d)*R_plus[i]) -
+                               atan(P0[i]*l_minus[i], R0[i]^2+abs(d)*R_minus[i])))
+    end
+    integral
+end
+
+# function computeSingularParameters()
--- tests/unit_tests/test_greens_functions.jl
+++ tests/unit_tests/test_greens_functions.jl
@@ -1,13 +1,26 @@
 using Test
 
 include("../../src/greens_functions.jl")
-#using ..greens_functions
 
 @testset "greens_functions tests" begin
-    @testset "scalar_greens tests" begin
-        @test scalar_greens(1, 0) == 1/(4*pi)
-        @test scalar_greens(1, 1) == exp(-im)/(4*pi)
-        @test scalar_greens(-1.5, 2.1) == exp(-im*2.1*1.5)/(4*pi*1.5)
-        @test isinf(scalar_greens(0, 100)) == true
+    @testset "scalarGreens tests" begin
+        @test scalarGreens(1.0, 0.0+0*im) == 1/(4*pi)
+        @test scalarGreens(1.0, 1.0+0*im) == exp(-im)/(4*pi)
+        @test scalarGreens(-1.5, 2.1+0*im) == exp(-im*2.1*1.5)/(4*pi*1.5)
+        @test isinf(scalarGreens(0.0, 100.0+0*im)) == true
+    end
+    @testset "singularScalarGreens" begin
+        d = -2.0
+        P0_hat = [1.0 0 0; 0 1.0 0; 0 0 1.0]
+        u_hat = [-1.0 0 0; 0 -1.0 0; 0 0 -1.0]
+        P0 = [2.0, 1.0, 1.5]
+        R0 = [1.2, 1.3, 1.4]
+        R_plus = [3.0, 4.0, 1.0]
+        R_minus = [1.0, 1.5, 2.1]
+        l_plus = [0.5, 0.1, 0.5]
+        l_minus = [0.2, 0.5, 0.2]
+        solution = -2.09660929985
+        @test isapprox(singularScalarGreens(d, P0_hat, u_hat, P0, R0, R_plus,
+                                            R_minus, l_plus, l_minus), solution)
     end
 end
