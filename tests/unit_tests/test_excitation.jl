69e23ea4813135d517ec8967065e98ceac2c8e3d
Jack Hamel <jackhaml16@gmail.com>
2020-11-10 17:57:44 -0700

    computation of spherical harmonics is there and tested. needs refactoring, then I need to add bessel functions to compute the spherical waves.

--- tests/unit_tests/test_excitation.jl
+++ tests/unit_tests/test_excitation.jl
@@ -1,4 +1,5 @@
 using Test
+using GSL
 
 include("../../src/excitation.jl")
 
@@ -23,5 +24,30 @@ include("../../src/excitation.jl")
         position = [0.0, 10.0, -3.2]
         solution = 9.77330616178066 + im*2.117188387484732
         @test isapprox(planeWave(amplitude, wavevector, position), solution, rtol=1e-15)
+    end
+    @testset "sphericalWave tests" begin
+        lmax = 1
+        l = [1, 1, 1]
+        m = [-1, 0, 1]
+        Plm_idxs = [3, 2, 3]
+        theta = pi/4
+        phi = pi/8
+
+        leg_polys, d_leg_polys = sf_legendre_deriv_array(GSL_SF_LEGENDRE_SPHARM, lmax, cos(theta))
+        Plm = leg_polys[Plm_idxs]
+        d_Plm = d_leg_polys[Plm_idxs]
+        phase_factors = [1, 1, -1]
+        exponentials = exp.(im.*m*phi)
+        sph_harms_sol = phase_factors .* Plm .* exponentials
+        dtheta_sph_harms_sol = -sin(theta) .* phase_factors .* d_Plm .* exponentials
+        dphi_sph_harms_sol = 1im .* m .* phase_factors .* Plm .* exponentials
+
+        sph_harms_test = Ylm_val(theta, phi, lmax)
+
+        @test isapprox(sph_harms_test[1], sph_harms_sol, rtol=1e-15)
+        @test isapprox(sph_harms_test[2], dtheta_sph_harms_sol, rtol=1e-15)
+        @test isapprox(sph_harms_test[3], dphi_sph_harms_sol, rtol=1e-15)
+        @test isapprox(sph_harms_test[4], l, rtol=1e-15)
+        @test isapprox(sph_harms_test[5], m, rtol=1e-15)
     end
 end
