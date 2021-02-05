2c8c52958b1471bbe1c4590ecc49bc09a7ca75d0
Jack Hamel <jackhaml16@gmail.com>
2021-02-04 21:38:25 -0500

    Everything works. need to cleanup and run sphere test

--- src/fill.jl
+++ src/fill.jl
@@ -2,29 +2,34 @@
 
 using LinearAlgebra
 
-function rhsFill(num_elements::Int64, elements::Array{Int64, 2}, nodes::Array{Float64, 2}, fieldFunc::Function, quadrature_rule::Array{Float64, 2})
+function rhsFill(pulse_mesh::PulseMesh, fieldFunc::Function)
+    @unpack num_elements,
+            elements,
+            nodes,
+            test_quadrature_points,
+            test_quadrature_weights = pulse_mesh
     rhs = Array{Complex{Float64}, 1}(undef, num_elements)
     for element_idx in 1:num_elements
         triangle_nodes = getTriangleNodes(element_idx, elements, nodes)
-        rhs[element_idx] = -1 * integrateTriangle(triangle_nodes, fieldFunc, quadrature_rule[:,1:3], quadrature_rule[:,4])
+        rhs[element_idx] = -1 * integrateTriangle(triangle_nodes, fieldFunc, test_quadrature_points[element_idx], test_quadrature_weights)
     end
     rhs
 end
 
-function matrixFill(num_elements::Int64, elements::Array{Int64, 2}, nodes::Array{Float64, 2}, testIntegrand::Function, quadrature_rule::Array{Float64, 2})
-    # @unpack num_elements,
-    #         elements,
-    #         nodes,
-    #         test_quadrature_points,
-    #         test_quadrature_weights = pulse_mesh
+function matrixFill(pulse_mesh::PulseMesh, testIntegrand::Function)
+    @unpack num_elements,
+            elements,
+            nodes,
+            test_quadrature_points,
+            test_quadrature_weights = pulse_mesh
     z_matrix = Array{Complex{Float64}, 2}(undef, num_elements, num_elements)
     for src_idx in 1:num_elements
         for test_idx in 1:num_elements
             is_singular = (src_idx == test_idx)
             test_nodes = getTriangleNodes(test_idx, elements, nodes)
             src_nodes = getTriangleNodes(src_idx, elements, nodes)
-            testIntegrandXYZ(x,y,z) = testIntegrand([x,y,z], src_nodes, is_singular)
-            z_matrix[test_idx, src_idx] = integrateTriangle(test_nodes, testIntegrandXYZ, quadrature_rule[:,1:3], quadrature_rule[:,4])
+            testIntegrandXYZ(x,y,z) = testIntegrand([x,y,z], src_idx, is_singular)
+            z_matrix[test_idx, src_idx] = integrateTriangle(test_nodes, testIntegrandXYZ, test_quadrature_points[test_idx], test_quadrature_weights)
         end
     end
     z_matrix
