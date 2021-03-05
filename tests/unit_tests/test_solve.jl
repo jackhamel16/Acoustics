748d15b5a8a91c31a10da8a8492f8f1624257fac
Jack Hamel <jackhaml16@gmail.com>
2021-03-04 19:28:42 -0500

    Unit test checking weighting functionality of cfie

---
+++ tests/unit_tests/test_solve.jl
@@ -0,0 +1,60 @@
+using Test
+using GSL
+
+include("../../src/includes.jl")
+
+@testset "solve tests" begin
+    @testset "solveSoftCFIE tests" begin
+        # All these test do is make sure CFIE is softIE with softIE_weight is 1
+        # and is softIENormalDeriv when softIE_weight is zeros
+        excitation_amplitude = 1.0
+        lambda=2.5
+        wavenumber = 2*pi/lambda + 0*im
+        wavevector = [wavenumber, 0.0, 0.0]
+        src_quadrature_rule = gauss7rule
+        test_quadrature_rule = gauss1rule
+        distance_to_edge_tol = 1e-12
+        near_singular_tol = 1.0
+
+        mesh_filename = "examples/simple/circular_plate_1m.msh"
+        planeWaveExcitation(x_test, y_test, z_test) = planeWave(excitation_amplitude, wavevector, [x_test,y_test,z_test])
+        planeWaveExcitationNormalDeriv(x_test, y_test, z_test, normal) = planeWaveNormalDerivative(excitation_amplitude, wavevector, [x_test,y_test,z_test], normal)
+
+        sourcesIE = solveSoftIE(mesh_filename,
+                        planeWaveExcitation,
+                        wavenumber,
+                        src_quadrature_rule,
+                        test_quadrature_rule,
+                        distance_to_edge_tol,
+                        near_singular_tol)
+        sourcesIEnd = solveSoftIENormalDeriv(mesh_filename,
+                        planeWaveExcitationNormalDeriv,
+                        wavenumber,
+                        src_quadrature_rule,
+                        test_quadrature_rule)
+        soft_IE_only = 1.0
+        soft_IE_nd_only = 0.0
+        sourcesCFIE_soft_IE_only = solveSoftCFIE(mesh_filename,
+                        planeWaveExcitation,
+                        planeWaveExcitationNormalDeriv,
+                        wavenumber,
+                        src_quadrature_rule,
+                        test_quadrature_rule,
+                        distance_to_edge_tol,
+                        near_singular_tol,
+                        soft_IE_only)
+        sourcesCFIE_soft_IE_nd_only = solveSoftCFIE(mesh_filename,
+                        planeWaveExcitation,
+                        planeWaveExcitationNormalDeriv,
+                        wavenumber,
+                        src_quadrature_rule,
+                        test_quadrature_rule,
+                        distance_to_edge_tol,
+                        near_singular_tol,
+                        soft_IE_nd_only)
+        @test isapprox(sourcesCFIE_soft_IE_only, sourcesIE, rtol=1e-12)
+        @test false==isapprox(sourcesCFIE_soft_IE_only, sourcesIEnd, rtol=1e-12)
+        @test isapprox(sourcesCFIE_soft_IE_nd_only, sourcesIEnd, rtol=1e-12)
+        @test false==isapprox(sourcesCFIE_soft_IE_nd_only, sourcesIE, rtol=1e-12)
+    end
+end
