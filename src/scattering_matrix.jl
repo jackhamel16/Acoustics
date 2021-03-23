9f2d6b508f438eccd6194e4ca4250ad2e5c99b2b
Jack Hamel <jackhaml16@gmail.com>
2021-03-23 13:33:10 -0400

    scattering matrix working and tests are all there. need to optimize and clean up

--- src/scattering_matrix.jl
+++ src/scattering_matrix.jl
@@ -1,4 +1,4 @@
-function calculateScatteringMatrix(max_l, wavenumber, excitation_amplitude, pulse_mesh, distance_to_edge_tol, near_singular_tol)
+function calculateScatteringMatrix(max_l, wavenumber, pulse_mesh, distance_to_edge_tol, near_singular_tol)
     num_harmonics = max_l^2 + 2*max_l + 1
     S_matrix = zeros(ComplexF64, num_harmonics, num_harmonics)
     # might be able to just loop through when delta function is 1 to save time
@@ -14,29 +14,51 @@ function calculateScatteringMatrix(max_l, wavenumber, excitation_amplitude, puls
         end # tm
     end # tl
 
-    VJ_matrix = calculateVJMatrix(max_l, wavenumber, excitation_amplitude, pulse_mesh, distance_to_edge_tol, near_singular_tol)
+    VJ_matrix = calculateVJMatrix(max_l, wavenumber, pulse_mesh, distance_to_edge_tol, near_singular_tol)
     S_matrix += im/(2*wavenumber) * VJ_matrix
 
     return(S_matrix)
 end
 
-function calculateVJMatrix(max_l, wavenumber, excitation_amplitude, pulse_mesh, distance_to_edge_tol, near_singular_tol)
+function calculateVJMatrix(max_l, wavenumber, pulse_mesh, distance_to_edge_tol, near_singular_tol)
     @unpack num_elements = pulse_mesh
+    excitation_amplitude = 1.0
     num_harmonics = max_l^2 + 2*max_l + 1
 
     V_matrix = Array{ComplexF64}(undef, num_harmonics, num_elements)
     J_matrix = Array{ComplexF64}(undef, num_elements, num_harmonics)
     harmonic_idx = 1
+    # compute Z
+    testIntegrand(r_test, src_idx, is_singular) = scalarGreensIntegration(pulse_mesh, src_idx,
+                                                   wavenumber,
+                                                   r_test,
+                                                   distance_to_edge_tol,
+                                                   near_singular_tol,
+                                                   is_singular)
+    println("Filling Z Matrix for Scattering Matrix...")
+    z_matrix = zeros(ComplexF64, num_elements, num_elements)
+    matrixFill(pulse_mesh, testIntegrand, z_matrix)
+    # facotrize Z
+    z_factors = factorize(z_matrix)
+    # for each RHS
+    println("Filling V and J Matrices for Scattering Matrix...")
     for l = 0:max_l
         for m=-l:l
-            sphericalWaveExcitation(x_test, y_test, z_test) = sphericalWave(excitation_amplitude, real(wavenumber), [x_test,y_test,z_test], l, m)
-            J, Z, RHS = solveSoftIE(pulse_mesh,
-                                 sphericalWaveExcitation,
-                                 wavenumber,
-                                 distance_to_edge_tol,
-                                 near_singular_tol,
-                                 true)
-            V_matrix[harmonic_idx,:] = RHS
+            # compute RHS
+            sphericalWaveExcitation(x_test, y_test, z_test) = 2 * wavenumber * sphericalWave(excitation_amplitude, real(wavenumber), [x_test,y_test,z_test], l, m)
+            rhs = zeros(ComplexF64, num_elements)
+            rhsFill(pulse_mesh, sphericalWaveExcitation, rhs)
+            # Store RHS in V matrix
+            V_matrix[harmonic_idx,:] = rhs
+            # compute J and sotre in J matrix
+            J = z_factors \ rhs
+            # J, Z, RHS = solveSoftIE(pulse_mesh,
+            #                      sphericalWaveExcitation,
+            #                      wavenumber,
+            #                      distance_to_edge_tol,
+            #                      near_singular_tol,
+            #                      true)
+            # V_matrix[harmonic_idx,:] = RHS
             J_matrix[:,harmonic_idx] = J
             harmonic_idx += 1
         end
--- tests/unit_tests/test_scattering_matrix.jl
+++ tests/unit_tests/test_scattering_matrix.jl
@@ -4,10 +4,25 @@ include("../../src/includes.jl")
 
 @testset "scattering_matrix tests" begin
     @testset "calculateScatteringMatrix tests" begin
-        # max_l = 1
-        # wavenumber = 1;
-        # num_harmonics = 4 #max_l^2 + 2*max_l + 1
-        # solution_S = [1 0 0 0; 0 0 0 -1; 0 0 1 0; 0 -1 0 0] + im*5*ones(4,4)
+        # Test exact solution for l=0
+        max_l = 0
+        lambda=20.0
+        wavenumber = 2*pi/lambda + 0*im
+        num_harmonics = 1
+        src_quadrature_rule = gauss7rule
+        test_quadrature_rule = gauss1rule
+        distance_to_edge_tol = 1e-12
+        near_singular_tol = 1.0
+        mesh_filename = "examples/test/sphere_1m_1266.msh"
+        a = 1.0 # sphere radius
+        pulse_mesh = buildPulseMesh(mesh_filename, src_quadrature_rule, test_quadrature_rule)
+        test_S = calculateScatteringMatrix(max_l, wavenumber, pulse_mesh, distance_to_edge_tol, near_singular_tol)
+        solution_S = 1 - 2 * sphericalBesselj(0, real(wavenumber) * a) / sphericalHankel2(0, real(wavenumber) * a)
+        @test Array{ComplexF64, 2} == typeof(test_S)
+        @test size(test_S) == (num_harmonics, num_harmonics)
+        @test isapprox(abs(test_S[1,1]), 1.0, rtol=1e-5)
+        @test isapprox(test_S[1,1], solution_S, rtol=0.25e-2)
+
         max_l = 1
         lambda=20.0
         wavenumber = 2*pi/lambda + 0*im
@@ -17,14 +32,13 @@ include("../../src/includes.jl")
         test_quadrature_rule = gauss1rule
         distance_to_edge_tol = 1e-12
         near_singular_tol = 1.0
-        # mesh_filename = "examples/test/rectangle_plate_8elements_symmetric.msh"
         mesh_filename = "examples/test/sphere_1m_1266.msh"
         pulse_mesh = buildPulseMesh(mesh_filename, src_quadrature_rule, test_quadrature_rule)
         V_solution = zeros(ComplexF64, num_harmonics, pulse_mesh.num_elements)
         J_solution = zeros(ComplexF64, pulse_mesh.num_elements, num_harmonics)
         l, m = 0, 0
         for harmonic_idx=1:num_harmonics
-            sphericalWaveExcitation(x_test, y_test, z_test) = sphericalWave(excitation_amplitude, real(wavenumber), [x_test,y_test,z_test], l, m)
+            sphericalWaveExcitation(x_test, y_test, z_test) = 2*wavenumber*sphericalWave(excitation_amplitude, real(wavenumber), [x_test,y_test,z_test], l, m)
             sources, z, rhs = solveSoftIE(pulse_mesh,
                               sphericalWaveExcitation,
                               wavenumber,
@@ -41,14 +55,33 @@ include("../../src/includes.jl")
             end
         end
         solution_S = [1 0 0 0; 0 0 0 -1; 0 0 1 0; 0 -1 0 0] + im/(2*wavenumber)*V_solution*J_solution
-        test_S = calculateScatteringMatrix(max_l, wavenumber, excitation_amplitude, pulse_mesh, distance_to_edge_tol, near_singular_tol)
+        no_coupling_matrix = [1 0 0 0; 0 0 0 1; 0 0 1 0; 0 1 0 0] # mag of zero in spots corresponding to coupling between different harmonics
+        test_S = calculateScatteringMatrix(max_l, wavenumber, pulse_mesh, distance_to_edge_tol, near_singular_tol)
         @test Array{ComplexF64, 2} == typeof(test_S)
-        @test (num_harmonics, num_harmonics) == size(test_S)
+        @test size(test_S) == (num_harmonics, num_harmonics)
         @test isapprox(solution_S, test_S, rtol=1e-14)
         @test isapprox(test_S, transpose(test_S), rtol=1e-7)
-        @test isapprox(1, abs(det(test_S) * det(adjoint(test_S))), rtol=1e-4)
+        @test isapprox(1, abs(det(test_S) * det(adjoint(test_S))), rtol=0.5e-4)
+        @test isapprox(round.(abs.(test_S)), no_coupling_matrix, rtol = 1e-14)
+
+        max_l = 8
+        lambda=10.0
+        wavenumber = 2*pi/lambda + 0*im
+        excitation_amplitude = 1.0
+        num_harmonics = 81
+        src_quadrature_rule = gauss7rule
+        test_quadrature_rule = gauss1rule
+        distance_to_edge_tol = 1e-12
+        near_singular_tol = 1.0
+        mesh_filename = "examples/test/sphere_1m_1266.msh"
+        pulse_mesh = buildPulseMesh(mesh_filename, src_quadrature_rule, test_quadrature_rule)
+
+        test_S = calculateScatteringMatrix(max_l, wavenumber, pulse_mesh, distance_to_edge_tol, near_singular_tol)
+        @test size(test_S) == (num_harmonics, num_harmonics)
+        @test isapprox(test_S, transpose(test_S), rtol=1e-5)
+        @test isapprox(1, abs(det(test_S) * det(adjoint(test_S))), rtol=1e-3)
     end # calculateScatteringMatrix tests
-    @testset "calculateVMatrix tests" begin
+    @testset "calculateVJMatrix tests" begin
         max_l = 1
         wavenumber = 1.0 + 0.0im
         excitation_amplitude = 1.0
@@ -64,7 +97,7 @@ include("../../src/includes.jl")
         J_solution = zeros(ComplexF64, num_elements, num_harmonics)
         l, m = 0, 0
         for harmonic_idx=1:num_harmonics
-            sphericalWaveExcitation(x_test, y_test, z_test) = sphericalWave(excitation_amplitude, real(wavenumber), [x_test,y_test,z_test], l, m)
+            sphericalWaveExcitation(x_test, y_test, z_test) = 2*wavenumber*sphericalWave(excitation_amplitude, real(wavenumber), [x_test,y_test,z_test], l, m)
             sources, z, rhs = solveSoftIE(pulse_mesh,
                               sphericalWaveExcitation,
                               wavenumber,
@@ -80,6 +113,9 @@ include("../../src/includes.jl")
                 m = -l
             end
         end
-        @test isapprox(V_solution * J_solution, calculateVJMatrix(max_l, wavenumber, excitation_amplitude, pulse_mesh, distance_to_edge_tol, near_singular_tol), rtol=1e-14)
-    end # calculateScatteringMatrix tests
+        test_VJ = calculateVJMatrix(max_l, wavenumber, pulse_mesh, distance_to_edge_tol, near_singular_tol)
+        solution_VJ = V_solution * J_solution
+        @test size(test_VJ) == (num_harmonics, num_harmonics)
+        @test isapprox(solution_VJ, test_VJ, rtol=1e-14)
+    end # calculateVJMatrix tests
 end # scattering_matrix tests
