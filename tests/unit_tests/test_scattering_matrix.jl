5ac209242a753f685834fb48454fd1b6a19f2627
Jack Hamel <jackhaml16@gmail.com>
2021-03-26 10:56:26 -0400

    added tests for non-sphere

--- tests/unit_tests/test_scattering_matrix.jl
+++ tests/unit_tests/test_scattering_matrix.jl
@@ -26,6 +26,7 @@ include("../../src/includes.jl")
         max_l = 1
         lambda=20.0
         wavenumber = 2*pi/lambda + 0*im
+        a = 1.0 # sphere radius
         excitation_amplitude = 1.0
         num_harmonics = 4
         src_quadrature_rule = gauss7rule
@@ -37,34 +38,21 @@ include("../../src/includes.jl")
         V_solution = zeros(ComplexF64, num_harmonics, pulse_mesh.num_elements)
         J_solution = zeros(ComplexF64, pulse_mesh.num_elements, num_harmonics)
         l, m = 0, 0
-        for harmonic_idx=1:num_harmonics
-            sphericalWaveExcitation(x_test, y_test, z_test) = 2*wavenumber*
-                                                              sphericalWave(excitation_amplitude,
-                                                                            real(wavenumber),
-                                                                            [x_test,y_test,z_test],
-                                                                            l,
-                                                                            m)
-            sources, z, rhs = solveSoftIE(pulse_mesh,
-                                          sphericalWaveExcitation,
-                                          wavenumber,
-                                          distance_to_edge_tol,
-                                          near_singular_tol,
-                                          true)
-            V_solution[harmonic_idx, :] = rhs
-            J_solution[:, harmonic_idx] = sources
-            if m < l
-                m += 1
-            else
-                l+= 1
-                m = -l
+        solution = zeros(ComplexF64, num_harmonics, num_harmonics)
+        for tl=0:max_l
+            pl = tl
+            for tm=-tl:tl
+                pm = -tm
+                t_idx = tl^2 + tl + tm + 1
+                p_idx = pl^2 + pl + pm + 1
+                solution[t_idx,p_idx] = (-1)^tm * (1 - 2 * sphericalBesselj(tl, real(wavenumber) * a) / sphericalHankel2(tl, real(wavenumber) * a))
             end
         end
-        solution_S = [1 0 0 0; 0 0 0 -1; 0 0 1 0; 0 -1 0 0] + im/(2*wavenumber)*V_solution*J_solution
         no_coupling_matrix = [1 0 0 0; 0 0 0 1; 0 0 1 0; 0 1 0 0] # mag of zero in spots corresponding to coupling between different harmonics
         test_S = calculateScatteringMatrix(max_l, wavenumber, pulse_mesh, distance_to_edge_tol, near_singular_tol)
         @test Array{ComplexF64, 2} == typeof(test_S)
         @test size(test_S) == (num_harmonics, num_harmonics)
-        @test isapprox(solution_S, test_S, rtol=1e-14)
+        @test isapprox(solution, test_S, rtol=1e-2)
         @test isapprox(test_S, transpose(test_S), rtol=1e-7)
         @test isapprox(1, abs(det(test_S) * det(adjoint(test_S))), rtol=0.5e-4)
         @test isapprox(abs.(test_S), no_coupling_matrix, rtol = 1e-5)
@@ -85,6 +73,25 @@ include("../../src/includes.jl")
         @test size(test_S) == (num_harmonics, num_harmonics)
         @test isapprox(test_S, transpose(test_S), rtol=1e-5)
         @test isapprox(1, abs(det(test_S) * det(adjoint(test_S))), rtol=1e-3)
+
+        #Test using a flat plate and S should still be unitary
+        max_l = 8
+        lambda=10.0
+        wavenumber = 2*pi/lambda + 0*im
+        excitation_amplitude = 1.0
+        num_harmonics = 81
+        src_quadrature_rule = gauss7rule
+        test_quadrature_rule = gauss1rule
+        distance_to_edge_tol = 1e-12
+        near_singular_tol = 1.0
+        mesh_filename = "examples/test/rectangle_plate.msh"
+        pulse_mesh = buildPulseMesh(mesh_filename, src_quadrature_rule, test_quadrature_rule)
+
+        test_S = calculateScatteringMatrix(max_l, wavenumber, pulse_mesh, distance_to_edge_tol, near_singular_tol)
+        @test size(test_S) == (num_harmonics, num_harmonics)
+        @test isapprox(test_S, transpose(test_S), rtol=1e-5)
+        @test isapprox(1, abs(det(test_S) * det(adjoint(test_S))), rtol=0.11e-2)
+
     end # calculateScatteringMatrix tests
     @testset "calculateVJMatrix tests" begin
         max_l = 1
