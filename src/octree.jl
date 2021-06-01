7765075373994009b9f80c79affbc3cd000e06df
Jack Hamel <jackhaml16@gmail.com>
2021-06-01 17:59:55 -0400

    intializing octree function complete. need to add functions to split nodes into children and store them properly

--- src/octree.jl
+++ src/octree.jl
@@ -1,29 +1,78 @@
 using LinearAlgebra
 using Parameters
 
-@with_kw mutable struct Octree
-    num_levels::Int64 = 0
-    # box_to_element_map::AbstractArray{Array{Int64,1},1} = [[]]
-    box_centroids::AbstractArray{Array{Array{Float64,1},1},1} = [[[]]]
-    box_bounds
+# @with_kw mutable struct Octree
+#     num_levels::Int64 = 0
+#     box_to_element_map::AbstractArray{Array{Array{Int64,1},1},1} = [[[]]]
+#     box_centroids::AbstractArray{Array{Array{Float64,1},1},1} = [[[]]]
+#     box_bounds::AbstractArray{Array{Array{Tuple{Float64,Float64},1},1},1} = [[[()]]]
+# end
+
+mutable struct Node
+    parent_idx::Int64
+    children_idxs::Array{Int64,1}
+    element_idxs::Array{Int64,1}
+    bounds::Array{Array{Float64,1},1}
+    centroid::Array{Float64,1}
 end
 
-function initializeOctree(num_levels, ele_centroids::AbstractArray{Array{Float64,1},1})
-    num_boxes = 8^(num_levels-1)
+mutable struct Octree
+    num_levels::Int64
+    top_node_idx::Int64
+    nodes::Array{Node,1}
+end
+
+function computeNodeBounds(half_edge_length, node_centroid::Array{Float64,1})
+    return([[node_centroid[1] - half_edge_length, node_centroid[1] + half_edge_length],
+            [node_centroid[2] - half_edge_length, node_centroid[2] + half_edge_length],
+            [node_centroid[3] - half_edge_length, node_centroid[3] + half_edge_length]])
+end
+
+function initializeOctree(num_levels::Int64, ele_centroids::AbstractArray{Array{Float64,1},1})
+    # Creates in octree instance with only the level 1 node, the node
+    # encapsulating all elements, filled.
+    no_parent_idx = 0 # the zero indicates it is at the highest level
+    no_children_idxs = [] # empty array indicates it is a leaf
     num_elements = length(ele_centroids)
-    lvl0_box_center = sum(ele_centroids)/num_elements
-    box_centroids = [[[0.0,0,0]]]
-    #find edge length of level 0 box
-    max_distance = 0.0
+    all_ele_idxs = [i for i=1:num_elements]
+    level1_centroid = sum(ele_centroids)/num_elements
+    max_distance = 0.0 # from centroid of node
     for ele_idx = 1:num_elements
-        distance = norm(lvl0_box_center - ele_centroids[ele_idx])
+        distance = norm(level1_centroid - ele_centroids[ele_idx])
         if distance > max_distance
             max_distance = distance
         end
     end
-    lvl0_edge_length = 2*max_distance
-# left off computing and storing box bounds
-    return(Octree(num_levels, box_centroids))
+    level1_edge_length = 2*max_distance
+    level1_bounds = computeNodeBounds(max_distance, level1_centroid)
+    level1_node = Node(no_parent_idx, no_children_idxs, all_ele_idxs, level1_bounds, level1_centroid)
+    level1_node_idx = 1
+    return(Octree(num_levels, level1_node_idx, [level1_node]))
+end # initializeOctree
+
+#     num_boxes = 8^(num_levels-1)
+#     num_elements = length(ele_centroids)
+#     lvl0_box_center = sum(ele_centroids)/num_elements
+#     box_centroids = [[[0.0,0,0]]]
+#     #find edge length of level 0 box
+#     max_distance = 0.0
+#     for ele_idx = 1:num_elements
+#         distance = norm(lvl0_box_center - ele_centroids[ele_idx])
+#         if distance > max_distance
+#             max_distance = distance
+#         end
+#     end
+#     lvl0_edge_length = 2*max_distance
+# # left off computing and storing box bounds
+#     lvl0_x_centroid = box_centroids[1][1][1]
+#     lvl0_y_centroid = box_centroids[1][1][2]
+#     lvl0_z_centroid = box_centroids[1][1][3]
+#     lvl0_x_bounds = (lvl0_x_centroid - max_distance, lvl0_x_centroid + max_distance)
+#     lvl0_y_bounds = (lvl0_y_centroid - max_distance, lvl0_y_centroid + max_distance)
+#     lvl0_z_bounds = (lvl0_z_centroid - max_distance, lvl0_z_centroid + max_distance)
+#     lvl0_bounds = [[lvl0_x_bounds, lvl0_y_bounds, lvl0_z_bounds]]
+#     box_bounds = [lvl0_bounds]
+#     return(Octree(num_levels=num_levels, box_centroids=box_centroids, box_bounds=box_bounds))
     # box_bounds = Array{Array{Float64,1},1}(undef, num_boxes)
 
     # octree = Octree()
@@ -38,4 +87,3 @@ function initializeOctree(num_levels, ele_centroids::AbstractArray{Array{Float64
     #     end
     # end
     # return(Octree(box_to_element_map, box_centroids))
-end
--- tests/unit_tests/test_octree.jl
+++ tests/unit_tests/test_octree.jl
@@ -21,12 +21,66 @@ include("../../src/octree.jl")
     # sol_map = [[],[],[],[],[],[],[],[]]
     # test_octree = initializeOctree(num_levels, ele_centroids)
     # @test isapprox(test_octree.box_to_element_map, sol_map, rtol=1e-15)
+    @testset "computeNodeBounds tests" begin
+        centroid = [0.0,0.0,0.0]
+        half_edge_length = 0.5
+        sol_bounds_xyz = (-0.5,0.5)
+        test_bounds = computeNodeBounds(half_edge_length, centroid)
+        @test isapprox(test_bounds[1][1], sol_bounds_xyz[1], rtol=1e-15)
+        @test isapprox(test_bounds[1][2], sol_bounds_xyz[2], rtol=1e-15)
+        @test isapprox(test_bounds[2][1], sol_bounds_xyz[1], rtol=1e-15)
+        @test isapprox(test_bounds[2][2], sol_bounds_xyz[2], rtol=1e-15)
+        @test isapprox(test_bounds[3][1], sol_bounds_xyz[1], rtol=1e-15)
+        @test isapprox(test_bounds[3][2], sol_bounds_xyz[2], rtol=1e-15)
+
+        centroid = [-0.5,0.0,1.0]
+        half_edge_length = 2.0
+        sol_bounds = [(-2.5,1.5),(-2,2),(-1,3)]
+        test_bounds = computeNodeBounds(half_edge_length, centroid)
+        @test isapprox(test_bounds[1][1], sol_bounds[1][1], rtol=1e-15)
+        @test isapprox(test_bounds[1][2], sol_bounds[1][2], rtol=1e-15)
+        @test isapprox(test_bounds[2][1], sol_bounds[2][1], rtol=1e-15)
+        @test isapprox(test_bounds[2][2], sol_bounds[2][2], rtol=1e-15)
+        @test isapprox(test_bounds[3][1], sol_bounds[3][1], rtol=1e-15)
+        @test isapprox(test_bounds[3][2], sol_bounds[3][2], rtol=1e-15)
+    end # computeNodeBounds tests
     @testset "initializeOctree tests" begin
         num_levels = 1
         ele_centroids = [[1.0,1,0],[-1.0,-1,0]]
-        box_centroids = [[[0,0,0]]]
+        node_centroid =[0.0,0.0,0.0]
+        box_bounds = [-sqrt(2),sqrt(2)] # same for all dimensions
+        sol_top_node_idx = 1
+        sol_num_nodes = 1
         test_octree = initializeOctree(num_levels, ele_centroids)
+        test_node = test_octree.nodes[1]
         @test isapprox(test_octree.num_levels, num_levels, rtol=1e-15)
-        @test isapprox(test_octree.box_centroids, box_centroids, rtol=1e-15)
+        @test isapprox(test_octree.top_node_idx, sol_top_node_idx, rtol=1e-15)
+        @test isapprox(length(test_octree.nodes), sol_num_nodes, rtol=1e-15)
+        @test isapprox(test_node.centroid, node_centroid, rtol=1e-15)
+        @test isapprox(test_node.parent_idx, 0, rtol=1e-15)
+        @test isempty(test_node.children_idxs)
+        @test isapprox(test_node.bounds[1][1], box_bounds[1], rtol=1e-15)
+        @test isapprox(test_node.bounds[1][2], box_bounds[2], rtol=1e-15)
+        @test isapprox(test_node.bounds[2][1], box_bounds[1], rtol=1e-15)
+        @test isapprox(test_node.bounds[2][2], box_bounds[2], rtol=1e-15)
+        @test isapprox(test_node.bounds[3][1], box_bounds[1], rtol=1e-15)
+        @test isapprox(test_node.bounds[3][2], box_bounds[2], rtol=1e-15)
+        #
+        num_levels = 3
+        ele_centroids = [[-1.0,0.2,0.0],[0.0,1.5,0.3],[0.0,0.0,1.0]]
+        node_centroid = [-1,1.7,1.3] ./ 3
+        max_ele_distance = 1.0
+        sol_x_bounds = [-1/3-max_ele_distance,-1/3+max_ele_distance]
+        sol_top_node_idx = 1
+        sol_num_nodes = 1
+        test_octree = initializeOctree(num_levels, ele_centroids)
+        test_node = test_octree.nodes[1]
+        @test isapprox(test_octree.num_levels, num_levels, rtol=1e-15)
+        @test isapprox(test_octree.top_node_idx, sol_top_node_idx, rtol=1e-15)
+        @test isapprox(length(test_octree.nodes), sol_num_nodes, rtol=1e-15)
+        @test isapprox(test_node.centroid, node_centroid, rtol=1e-15)
+        @test isapprox(test_node.parent_idx, 0, rtol=1e-15)
+        @test isempty(test_node.children_idxs)
+        @test isapprox(test_node.bounds[1], sol_x_bounds, rtol=1e-15)
     end # initializeOctree tests
 end # octree tests
