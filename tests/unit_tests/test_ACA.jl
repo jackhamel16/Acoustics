6278e6d747a077e68370974bfad83b147109c2a3
Jack Hamel <jackhaml16@gmail.com>
2021-06-15 17:44:55 -0400

    ACA algorithm in. Good to close

--- tests/unit_tests/test_ACA.jl
+++ tests/unit_tests/test_ACA.jl
@@ -67,33 +67,14 @@ include("../../src/ACA.jl")
         @test isapprox(V_vec, sol_V_vec, rtol=1e-15)
     end #computeRHSContribution tests
     @testset "computeRHSContributionACA" begin
-        # src_quadrature_rule = gauss7rule
-        # test_quadrature_rule = gauss7rule
-        # approximation_tol = 1e-3
-        # mesh_filename = "examples/test/rectangle_plate_8elements_symmetric.msh"
-        # pulse_mesh =  buildPulseMesh(mesh_filename, src_quadrature_rule, test_quadrature_rule)
-        # num_levels = 3
-        # octree = createOctree(num_levels, pulse_mesh)
-        # testIntegrand(r_test, src_idx, is_singular) = scalarGreensIntegration(pulse_mesh, src_idx,
-        #                                                wavenumber,
-        #                                                r_test,
-        #                                                distance_to_edge_tol,
-        #                                                near_singular_tol,
-        #                                                is_singular)
-        # z_matrix = zeros(ComplexF64, pulse_mesh.num_elements, pulse_mesh.num_elements)
-        # matrixFill(pulse_mesh, testIntegrand, z_matrix)
-        # test_node = octree.nodes[6]; src_node = octree.nodes[7]
-        # sol_sub_Z = z_matrix[test_node.element_idxs,src_node.element_idxs]
-        # function identityMatrix(global_test_ele_idx, global_src_ele_idxs)
-        #     row_or_col = zeros(Int64, length(global_src_ele_idxs))
-        #     for idx = 1:length(global_src_ele_idxs)
-        #         row_or_col[idx] = global_test_ele_idx == global_src_ele_idxs[idx]
-        #     end
-        #     return(row_or_col)
-        # end
-        # test_U, test_V = computeRHSContributionACA(identityMatrix, approximation_tol, test_node, src_node)
-        # @test isapprox(test_U*test_V, sol_sub_Z, rtol=0.26)
+        approximation_tol = 1e-3
+        num_rows, num_cols = 5, 5
+        sol = I(num_rows)
+        computeMatrixEntry(test_idx, src_idx) = convert(Float64, test_idx == src_idx)
+        test_U, test_V = computeRHSContributionACA(computeMatrixEntry, approximation_tol, num_rows, num_cols)
+        @test isapprox(test_U*test_V, sol, rtol=1e-14)
 
+        # nodes are not well-separated for this test
         wavenumber = 1.0+0.0im
         src_quadrature_rule = gauss7rule
         test_quadrature_rule = gauss7rule
@@ -114,9 +95,9 @@ include("../../src/ACA.jl")
         matrixFill(pulse_mesh, testIntegrand, z_matrix)
         test_node = octree.nodes[6]; src_node = octree.nodes[7]
         sol_sub_Z = z_matrix[test_node.element_idxs,src_node.element_idxs]
-        computeMatrixArrayFunc(global_test_ele_idx, global_src_ele_idxs) = computeZArray(pulse_mesh, wavenumber, distance_to_edge_tol, near_singular_tol, global_test_ele_idx, global_src_ele_idxs)
-        test_U, test_V = computeRHSContributionACA(computeMatrixArrayFunc, approximation_tol, test_node, src_node)
-        @test isapprox(test_U*test_V, sol_sub_Z, rtol=0.26)
+        computeMatrixEntry(test_idx,src_idx)=computeZEntrySoundSoft(pulse_mesh, test_node, src_node, wavenumber, distance_to_edge_tol, near_singular_tol, test_idx, src_idx)
+        test_U, test_V = computeRHSContributionACA(computeMatrixEntry, approximation_tol, length(test_node.element_idxs), length(src_node.element_idxs))
+        @test isapprox(test_U*test_V, sol_sub_Z, rtol=1e-14)
 
         # in this test, triangles are separated by ~80 elements
         wavenumber = 1.0+0.0im
@@ -124,8 +105,8 @@ include("../../src/ACA.jl")
         test_quadrature_rule = gauss7rule
         distance_to_edge_tol = 1e-12
         near_singular_tol = 1.0
-        approximation_tol = 1e-4 # uses maximum rank to approximate Z
-        mesh_filename = "examples/simple/disjoint_triangles.msh"
+        approximation_tol = 1e-4
+        mesh_filename = "examples/test/disjoint_triangles.msh"
         pulse_mesh =  buildPulseMesh(mesh_filename, src_quadrature_rule, test_quadrature_rule)
         num_levels = 3
         octree = createOctree(num_levels, pulse_mesh)
@@ -139,13 +120,18 @@ include("../../src/ACA.jl")
         matrixFill(pulse_mesh, testIntegrand, z_matrix)
         test_node = octree.nodes[6]; src_node = octree.nodes[7]
         sol_sub_Z = z_matrix[test_node.element_idxs,src_node.element_idxs]
-        computeMatrixArrayFunc(global_test_ele_idx, global_src_ele_idxs) = computeZArray(pulse_mesh, wavenumber, distance_to_edge_tol, near_singular_tol, global_test_ele_idx, global_src_ele_idxs)
-        test_U, test_V = computeRHSContributionACA(computeMatrixArrayFunc, approximation_tol, test_node, src_node)
-        @test isapprox(test_U*test_V, sol_sub_Z, rtol=0.29e-2)
+        singular_vals = svd(sol_sub_Z).S
+        # computeMatrixArrayFunc(global_test_ele_idx, global_src_ele_idxs) = computeZArray(pulse_mesh, wavenumber, distance_to_edge_tol, near_singular_tol, global_test_ele_idx, global_src_ele_idxs)
+        # test_U, test_V = computeRHSContributionACA(computeMatrixArrayFunc, approximation_tol, test_node, src_node)
+        computeMatrixEntry(test_idx,src_idx)=computeZEntrySoundSoft(pulse_mesh, test_node, src_node, wavenumber, distance_to_edge_tol, near_singular_tol, test_idx, src_idx)
+        test_U, test_V = computeRHSContributionACA(computeMatrixEntry, approximation_tol, length(test_node.element_idxs), length(src_node.element_idxs))
+        @test isapprox(test_U*test_V, sol_sub_Z, rtol=0.17e-5)
         test_node = octree.nodes[6]; src_node = octree.nodes[9]
         sol_sub_Z = z_matrix[test_node.element_idxs,src_node.element_idxs]
-        test_U, test_V = computeRHSContributionACA(computeMatrixArrayFunc, approximation_tol, test_node, src_node)
-        @test isapprox(test_U*test_V, sol_sub_Z, rtol=0.37e-2)
+        # test_U, test_V = computeRHSContributionACA(computeMatrixArrayFunc, approximation_tol, test_node, src_node)
+        computeMatrixEntry(test_idx,src_idx)=computeZEntrySoundSoft(pulse_mesh, test_node, src_node, wavenumber, distance_to_edge_tol, near_singular_tol, test_idx, src_idx)
+        test_U, test_V = computeRHSContributionACA(computeMatrixEntry, approximation_tol, length(test_node.element_idxs), length(src_node.element_idxs))
+        @test isapprox(test_U*test_V, sol_sub_Z, rtol=0.22e-5)
 
         # in this test, triangles are separated by ~800 elements
         wavenumber = 1.0+0.0im
@@ -153,8 +139,8 @@ include("../../src/ACA.jl")
         test_quadrature_rule = gauss7rule
         distance_to_edge_tol = 1e-12
         near_singular_tol = 1.0
-        approximation_tol = 1e-4 # uses maximum rank to approximate Z
-        mesh_filename = "examples/simple/far_apart_triangles.msh"
+        approximation_tol = 1e-16 # uses maximum rank to approximate Z
+        mesh_filename = "examples/test/far_apart_triangles.msh"
         pulse_mesh =  buildPulseMesh(mesh_filename, src_quadrature_rule, test_quadrature_rule)
         num_levels = 5
         octree = createOctree(num_levels, pulse_mesh)
@@ -169,14 +155,15 @@ include("../../src/ACA.jl")
         test_node = octree.nodes[14]; src_node = octree.nodes[17]
         # test_node = octree.nodes[13]; src_node = octree.nodes[11]
         sol_sub_Z = z_matrix[test_node.element_idxs,src_node.element_idxs]
-        computeMatrixArrayFunc(global_test_ele_idx, global_src_ele_idxs) = computeZArray(pulse_mesh, wavenumber, distance_to_edge_tol, near_singular_tol, global_test_ele_idx, global_src_ele_idxs)
-        test_U, test_V = computeRHSContributionACA(computeMatrixArrayFunc, approximation_tol, test_node, src_node)
-        @test isapprox(test_U*test_V, sol_sub_Z, rtol=0.5e-3)
-
+        computeMatrixEntry(test_idx,src_idx)=computeZEntrySoundSoft(pulse_mesh, test_node, src_node, wavenumber, distance_to_edge_tol, near_singular_tol, test_idx, src_idx)
+        test_U, test_V = computeRHSContributionACA(computeMatrixEntry, approximation_tol, length(test_node.element_idxs), length(src_node.element_idxs))
+        # computeMatrixArrayFunc(global_test_ele_idx, global_src_ele_idxs) = computeZArray(pulse_mesh, wavenumber, distance_to_edge_tol, near_singular_tol, global_test_ele_idx, global_src_ele_idxs)
+        # test_U, test_V = computeRHSContributionACA(computeMatrixArrayFunc, approximation_tol, test_node, src_node)
+        @test isapprox(test_U*test_V, sol_sub_Z, rtol=1e-15)
 
         # println(norm(test_U*test_V-sol_sub_Z)/norm(sol_sub_Z))
     end #computeRHSContributionACA
-    @testset "computeZArray tests" begin
+    @testset "computeZEntrySoundSoft tests" begin
         wavenumber = 1.0+0.0im
         src_quadrature_rule = gauss7rule
         test_quadrature_rule = gauss7rule
@@ -194,14 +181,20 @@ include("../../src/ACA.jl")
         z_matrix = zeros(ComplexF64, pulse_mesh.num_elements, pulse_mesh.num_elements)
         matrixFill(pulse_mesh, testIntegrand, z_matrix)
         global_test_idx = 1
-        global_src_idxs = [1]
-        test_array = computeZArray(pulse_mesh, wavenumber, distance_to_edge_tol, near_singular_tol, global_test_idx, global_src_idxs)
-        @test isapprox(test_array, z_matrix[global_test_idx, global_src_idxs], rtol=1e-14)
+        global_src_idx = 1
+        test_Z_entry = computeZEntrySoundSoft(pulse_mesh, wavenumber, distance_to_edge_tol, near_singular_tol, global_test_idx, global_src_idx)
+        @test isapprox(test_Z_entry, z_matrix[global_test_idx, global_src_idx], rtol=1e-14)
         global_test_idx = 2
-        global_src_idxs = [1,3,5]
-        test_array = computeZArray(pulse_mesh, wavenumber, distance_to_edge_tol, near_singular_tol, global_test_idx, global_src_idxs)
-        @test isapprox(test_array, z_matrix[global_test_idx, global_src_idxs], rtol=1e-14)
-    end
+        global_src_idx = 5
+        test_Z_entry = computeZEntrySoundSoft(pulse_mesh, wavenumber, distance_to_edge_tol, near_singular_tol, global_test_idx, global_src_idx)
+        @test isapprox(test_Z_entry, z_matrix[global_test_idx, global_src_idx], rtol=1e-14)
+        num_levels = 1
+        octree = createOctree(num_levels, pulse_mesh)
+        test_idx = 1
+        src_idx = 1
+        test_Z_entry = computeZEntrySoundSoft(pulse_mesh, octree.nodes[1], octree.nodes[1], wavenumber, distance_to_edge_tol, near_singular_tol, test_idx, src_idx)
+        @test isapprox(test_Z_entry, z_matrix[test_idx, src_idx], rtol=1e-14)
+    end # computeZEntrySoundSoft tests
     @testset "computeZJMatVec tests" begin
         wavenumber = 1.0+0.0im
         src_quadrature_rule = gauss7rule
