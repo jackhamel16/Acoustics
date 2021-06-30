ba43b37e2328c71b62df617ccc7ef7dc3298ff03
Jack Hamel <jackhaml16@gmail.com>
2021-06-30 14:11:30 -0400

    clean up. tests pass. ready to merge

--- tests/unit_tests/test_octree.jl
+++ tests/unit_tests/test_octree.jl
@@ -9,24 +9,6 @@ include("../../src/ACA.jl")
 include("../../src/octree.jl")
 
 @testset "octree tests" begin
-    # num_levels = 1
-    # ele_centroids = [[1.0,1,0],[-1.0,-1,0]]
-    # sol_map = [[1,2]]
-    # test_octree = initializeOctree(num_levels, ele_centroids)
-    # @test isapprox(test_octree.box_to_element_map, sol_map, rtol=1e-15)
-    #
-    # num_levels = 1
-    # ele_centroids = [[1.0,1,0],[-1.0,-1,0],[-1.0,1,0]]
-    # sol_map = [[1,2,3]]
-    # test_octree = initializeOctree(num_levels, ele_centroids)
-    # @test isapprox(test_octree.box_to_element_map, sol_map, rtol=1e-15)
-
-    # num_levels = 2
-    # ele_centroids = [[1,1,0.1],[-1,-1,-0.1]]
-    # sol_map = [[2],[],[],[],[],[],[],[8]]
-    # sol_map = [[],[],[],[],[],[],[],[]]
-    # test_octree = initializeOctree(num_levels, ele_centroids)
-    # @test isapprox(test_octree.box_to_element_map, sol_map, rtol=1e-15)
     @testset "computeNodeBounds tests" begin
         centroid = [0.0,0.0,0.0]
         half_edge_length = 0.5
