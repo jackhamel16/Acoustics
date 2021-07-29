50d10198d2e80abf706465ded1a09d138f707ffb
Jack Hamel <jackhaml16@gmail.com>
2021-07-29 16:02:01 -0400

    need to add ACA parsing function then done maybe

--- src/code_structures/input_params.jl
+++ src/code_structures/input_params.jl
@@ -1,3 +1,4 @@
+using LinearAlgebra
 using Parameters
 
 @with_kw struct ExcitationParams
@@ -10,17 +11,81 @@ using Parameters
     m::Int64 = 0
 end
 
+@with_kw struct ACAParams
+    num_levels::Int64 = 0
+    compression_distance::Float64 = 0.0
+    approximation_tol::Float64 = 0.0
+end
+
 @with_kw struct InputParams
     mesh_filename::String = ""
+    equation::String = ""
     src_quadrature_rule::String = ""
     test_quadrature_rule::String = ""
     excitation_params::ExcitationParams = ExcitationParams()
     distance_to_edge_tol::Float64 = 1e-12
     near_singular_tol::Float64 = 1.0
+    use_ACA::Bool = false
+    ACA_params::ACAParams = ACAParams()
 end
 
-@with_kw struct ACAParams
-    num_levels::Int64 = 0
-    compression_distance::Float64 = 1.5 # default compresses when nodes dont shre edge/corner
-    approximation_tol::Float64 = 1e-4
+function parseACAParams(input_file_lines::AbstractArray{T,1}) where T <: AbstractString
+
+end #parseACAParams
+
+function parseExcitationParams(input_file_lines::AbstractArray{T,1}) where T <: AbstractString
+    excitation_idx = findall(x -> x == "\nexcitation:",input_file_lines)[1]
+    type = getAttribute(input_file_lines[excitation_idx+1])
+    lambda = parse(Float64, getAttribute(input_file_lines[excitation_idx+2]))
+    amplitude = parse(Float64, getAttribute(input_file_lines[excitation_idx+3]))
+    wavevector_raw = parse.(Float64, split(getAttribute(input_file_lines[excitation_idx+4]), ' '))
+    wavenumber = 2 * pi / lambda
+    wavevector = wavenumber .* wavevector_raw ./ norm(wavevector_raw)
+    if type == "sphericalwave"
+        l = parse(Int64, getAttribute(input_file_lines[excitation_idx+5]))
+        m = parse(Int64, getAttribute(input_file_lines[excitation_idx+6]))
+        return(ExcitationParams(type=type,
+                                lambda=lambda,
+                                amplitude=amplitude,
+                                wavenumber=wavenumber,
+                                wavevector=wavevector,
+                                l=l,
+                                m=m))
+    end
+    if type == "planewave"
+        return(ExcitationParams(type=type,
+                                lambda=lambda,
+                                amplitude=amplitude,
+                                wavenumber=wavenumber,
+                                wavevector=wavevector))
+    end
+end #parseExcitationParams
+
+function parseInputParams(inputs_filename::String)
+    file = open(inputs_filename, "r")
+    file_lines = split(read(file, String), "\r")
+    mesh_filename = getAttribute(file_lines[1])
+    equation = getAttribute(file_lines[2])
+    src_quadrature_string = getAttribute(file_lines[3])
+    test_quadrature_string = getAttribute(file_lines[4])
+    distance_to_edge_tol = parse(Float64, getAttribute(file_lines[5]))
+    near_singular_tol = parse(Float64, getAttribute(file_lines[6]))
+    excitation_params = parseExcitationParams(file_lines)
+    use_ACA = getAttribute(file_lines[12]) == "yes"
+    if use_ACA == true
+        #add ACA parsing here
+    end
+    return(mesh_filename,
+           equation,
+           src_quadrature_string,
+           test_quadrature_string,
+           distance_to_edge_tol,
+           near_singular_tol,
+           excitation_params,
+           use_ACA)
+end # parseInputParams
+
+function getAttribute(input_file_line::AbstractString)
+    # separates extra wording and symbols from input parameter
+    return(strip(split(input_file_line, ":")[2], ' '))
 end
--- tests/unit_tests/input_params_tests.jl
+++ tests/unit_tests/input_params_tests.jl
@@ -5,19 +5,27 @@ include("../../src/code_structures/input_params.jl")
 @testset "input_params tests" begin
     @testset "InputParams tests" begin
         mesh_filename = "test_filename"
+        equation = "sound soft IE"
         src_quadrature_rule = "gauss7"
         test_quadrature_rule = "gauss1"
         excitation_params = ExcitationParams()
+        use_ACA = false
+        ACA_params = ACAParams()
         test_params = InputParams(mesh_filename=mesh_filename,
+                                  equation=equation,
                                   src_quadrature_rule=src_quadrature_rule,
                                   test_quadrature_rule=test_quadrature_rule,
-                                  excitation_params=excitation_params)
+                                  excitation_params=excitation_params,
+                                  use_ACA=use_ACA)
         @test test_params.mesh_filename == mesh_filename
+        @test test_params.equation == equation
         @test test_params.src_quadrature_rule == src_quadrature_rule
         @test test_params.test_quadrature_rule == test_quadrature_rule
         @test test_params.excitation_params == excitation_params
         @test isapprox(test_params.distance_to_edge_tol, 1e-12, rtol = 1e-14)
         @test isapprox(test_params.near_singular_tol, 1.0, rtol = 1e-14)
+        @test test_params.use_ACA == use_ACA
+        @test test_params.ACA_params == ACA_params
     end # InputParams tests
     @testset "ExcitationParams tests" begin
         excitation_type = "planewave"
@@ -49,6 +57,53 @@ include("../../src/code_structures/input_params.jl")
                                     compression_distance=test_compression_distance)
         @test isapprox(test_ACA_params.num_levels, test_num_levels, rtol = 1e-14)
         @test isapprox(test_ACA_params.compression_distance, test_compression_distance, rtol = 1e-14)
-        @test isapprox(test_ACA_params.approximation_tol, 1e-4, rtol = 1e-14)
+        @test isapprox(test_ACA_params.approximation_tol, 0, rtol = 1e-14)
     end # ExcitationParams tests
+    @testset "parseACAParams tests" begin
+
+    end #parseACAParams
+    @testset "parseExcitationParams tests" begin
+        inputs_filename = "examples/test/test_inputs1.txt"
+        file = open(inputs_filename, "r")
+        file_lines = split(read(file, String), "\r")
+        excitation_params = parseExcitationParams(file_lines)
+        @test excitation_params.type == "planewave"
+        @test isapprox(excitation_params.lambda, 1.1, rtol=1e-14)
+        @test isapprox(excitation_params.amplitude, -1.5, rtol=1e-14)
+        @test isapprox(excitation_params.wavenumber, 2 * pi / 1.1, rtol=1e-14)
+        @test isapprox(excitation_params.wavevector, [ 2 * pi / 1.1, 0, 0], rtol=1e-14)
+        @test isapprox(excitation_params.l, 0, rtol=1e-14)
+        @test isapprox(excitation_params.m, 0, rtol=1e-14)
+
+        inputs_filename = "examples/test/test_inputs2.txt"
+        file = open(inputs_filename, "r")
+        file_lines = split(read(file, String), "\r")
+        excitation_params = parseExcitationParams(file_lines)
+        @test excitation_params.type == "sphericalwave"
+        @test isapprox(excitation_params.lambda, 1.1, rtol=1e-14)
+        @test isapprox(excitation_params.amplitude, -1.5, rtol=1e-14)
+        @test isapprox(excitation_params.wavenumber, 2 * pi / 1.1, rtol=1e-14)
+        @test isapprox(excitation_params.wavevector, [ 2 * pi / 1.1, 0, 0], rtol=1e-14)
+        @test isapprox(excitation_params.l, 2, rtol=1e-14)
+        @test isapprox(excitation_params.m, 1, rtol=1e-14)
+    end # parseExcitationParams tests
+    @testset "parseInputParams tests" begin
+        filename = "examples/test/test_inputs1.txt"
+        excitation_params = parseExcitationParams(split(read(open(inputs_filename, "r"), String), "\r"))
+        test = parseInputParams(filename)
+        @test test[1] == "gibberish.msh"
+        @test test[2] == "sound soft IE"
+        @test test[3] == "gauss7"
+        @test test[4] == "gauss1"
+        @test test[5] == 1e-10
+        @test test[6] == 2.2
+        @test test[7].type == "planewave"
+        @test isapprox(test[7].lambda, 1.1, rtol=1e-14)
+        @test isapprox(test[7].amplitude, -1.5, rtol=1e-14)
+        @test isapprox(test[7].wavenumber, 2 * pi / 1.1, rtol=1e-14)
+        @test isapprox(test[7].wavevector, [2 * pi / 1.1, 0, 0], rtol=1e-14)
+        @test isapprox(test[7].l, 0, rtol=1e-14)
+        @test isapprox(test[7].m, 0, rtol=1e-14)
+        @test test[8] == false
+    end # parseInputParams tests
 end #input_params tests
