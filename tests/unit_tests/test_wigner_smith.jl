03780084075b5b59f25cb7391df8fc67b131d488
Jack Hamel <jackhaml16@gmail.com>
2021-10-25 17:58:24 -0400

    Update test_wigner_smith.jl
    updated WS tests

--- tests/unit_tests/test_wigner_smith.jl
+++ tests/unit_tests/test_wigner_smith.jl
@@ -312,17 +312,26 @@ include("../../src/includes.jl")
         test_quadrature_rule = gauss7rule
         distance_to_edge_tol = 1e-12
         near_singular_tol = 1.0
-        softIE_weight = 0.5
         num_levels = 3
         compression_distance = 1.5
         ACA_approximation_tol = 1e-5
         mesh_filename = "examples/test/spheres/sphere_1m_1266.msh"#"examples/test/circular_plate_1m.msh"
+        pulse_mesh_IE = buildPulseMesh(mesh_filename, src_quadrature_rule, test_quadrature_rule)
+        sources_IE = solveWSModeSoftACA(max_l, mode_idx, wavenumber, pulse_mesh_IE, distance_to_edge_tol, near_singular_tol, num_levels, compression_distance, ACA_approximation_tol)
+
+        softIE_weight = 1.0
         pulse_mesh = buildPulseMesh(mesh_filename, src_quadrature_rule, test_quadrature_rule)
         test_sources = solveWSModeSoftCFIEACA(max_l, mode_idx, wavenumber, pulse_mesh, distance_to_edge_tol, near_singular_tol, softIE_weight, num_levels, compression_distance, ACA_approximation_tol)
-        pulse_mesh_IE = buildPulseMesh(mesh_filename, src_quadrature_rule, test_quadrature_rule)
-        test_sources_IE = solveWSModeSoftACA(max_l, mode_idx, wavenumber, pulse_mesh_IE, distance_to_edge_tol, near_singular_tol, num_levels, compression_distance, ACA_approximation_tol)
-        @test_skip isapprox(test_sources[1][1], -0.5313576866296218 - 0.21990746085878485im, rtol=1e-5)
-        @test_skip isapprox(test_sources[1][468], -0.6792050632304026 - 0.28109552129052373im, rtol=0.8e-6)
+        @test isapprox(test_sources[1][1], sources_IE[1][1], rtol=1e-14)
+        @test isapprox(test_sources[1][468], sources_IE[1][468], rtol=1e-14)
+
+        softIE_weight = 0.0
+        pulse_mesh = buildPulseMesh(mesh_filename, src_quadrature_rule, test_quadrature_rule)
+        test_sources = solveWSModeSoftCFIEACA(max_l, mode_idx, wavenumber, pulse_mesh, distance_to_edge_tol, near_singular_tol, softIE_weight, num_levels, compression_distance, ACA_approximation_tol)
+        @test isapprox(test_sources[1][1], sources_IE[1][1], rtol=1e-1)
+        @test isapprox(test_sources[1][468], sources_IE[1][468], rtol=1e-1)
+        # @test_skip isapprox(test_sources[1][1], -0.5313576866296218 - 0.21990746085878485im, rtol=1e-5)
+        # @test_skip isapprox(test_sources[1][468], -0.6792050632304026 - 0.28109552129052373im, rtol=0.8e-6)
     end # solveWSModeSoftCFIEACA tests
     @testset "sphericalWaveWSMode tests" begin
         wavenumber = 2.0
